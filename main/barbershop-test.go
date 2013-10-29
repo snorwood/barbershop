e6ed876538a0500d84e08b6cd5dae99f25026058
snorwood <steven@snorwood.com>
2013-10-29 00:18:02 -0400

    IT WORKS

--- main/barbershop-test.go
+++ main/barbershop-test.go
@@ -1,75 +1,120 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"math/rand"
+	"os"
 	"time"
 )
 
-type person struct{
-	bool Busy
-	string Text 
+type Person struct {
+	Busy bool
+	ID   int
 }
 
-func spawnCustomer(entrance chan person) {
-	for i := 0; i < 30; i++ {
-		random := rand.Int(10) * time.Second
-		<- time.After(random)
+type MyWriter struct {
+	count   int
+	outputs []io.Writer
+}
 
-		if (cap(line) < len(line))
-		{
-			person := Person{}
-			entrance <- person
-		}
+func (self *MyWriter) Write(p []byte) (int, error) {
+	self.count += 1
+	s := fmt.Sprintf("%d.\t %s\n", self.count, string(p))
+
+	for _, output := range self.outputs {
+		fmt.Fprint(output, s)
 	}
+	return len(p), nil
+}
+
+func NewMyWriter(outputs ...io.Writer) *MyWriter {
+	writer := MyWriter{outputs: outputs}
+	return &writer
+}
+
+var current = time.Now()
+var log *os.File
+var output io.Writer
+
+func spawnCustomer(entrance chan *Person, done chan bool) {
+	for i := 1; i <= 30; i++ {
+		random := time.Duration(rand.Int31n(2)) * time.Second
+		<-time.After(random)
+		person := &Person{ID: i}
+		entrance <- person
+		fmt.Fprint(output, "Added new Customer ", i)
+	}
+
+	done <- true
 }
 
 func main() {
-	barbers = make([]person, 3)
-	line = make([]person, 0, 10)
+	os.Create("output.txt")
+	log, _ := os.OpenFile("output.txt", os.O_APPEND, 0666)
+	defer log.Close() //we'll close this file as we leave scope, no matter what
 
-	entrance := make(chan person)
+	output = NewMyWriter(os.Stdout, log)
+
+	barbers := make([]*Person, 3)
+	line := make([]*Person, 0, 10)
+
+	entrance := make(chan *Person)
 	doneCutting := make(chan chan bool)
+	done := make(chan bool)
+	go spawnCustomer(entrance, done)
+	running := true
 
-	go spawnCustomer(entrance)
-
-	select {
-	case person := <-entrance:
-		for _, barber := range(barbers)
-		{
-			if (!barber.Busy)
-			{
-				barber.Text = "c"
-				barber.Busy = true
-				go func f() {
-					<- time.After((rand.int(3) + 3) * time.Second)
-					c := make(chan bool)
-					doneCutting <- c
-					sleep := !(<- c)
-					if (sleep)
-					{
-						barber.Text = "z"
-						barber.Busy = false
-					} else {
-						barber.Busy = true
-						barber.Text = "c"
-						f()
-					}
-				}()
-			} else {
-				
-			}
-		}
-	case c := <-doneCutting:
-		if (cap(line) > 0)
-		{
-			c <- true
-			barber.Busy = true
-			barber.Text = "c"
-		}
-		else
-		{
-			c <- false
-		}
+	for i := range barbers {
+		barbers[i] = &Person{Busy: false, ID: i}
 	}
 
+	for running {
+		select {
+		case person := <-entrance:
+			spotAvailable := false
+			for _, barber := range barbers {
+				if !barber.Busy {
+					spotAvailable = true
+					var f func()
+					f = func() {
+						barber.Busy = true
+						fmt.Fprint(output, "Barber ", barber.ID, " is gettin' to work on Customer ", person.ID)
+						<-time.After(time.Duration(rand.Int31n(3)+39) * time.Second)
+						c := make(chan bool)
+						doneCutting <- c
+						fmt.Fprint(output, "Barber ", barber.ID, " finished haircut on Customer ", person.ID)
+						sleep := !(<-c)
+						if sleep {
+							fmt.Fprint(output, "Barber ", barber.ID, " is snoozin'")
+							barber.Busy = false
+						} else {
+							f()
+						}
+					}
+					go f()
+					break
+				}
+			}
+
+			if !spotAvailable {
+				if len(line) < cap(line) {
+					fmt.Fprint(output, "Customer ", person.ID, " got in line")
+					line = append(line, person)
+				} else {
+					fmt.Fprint(output, "Customer ", person.ID, " has been turned away")
+				}
+			}
+		case c := <-doneCutting:
+			if len(line) > 0 {
+				line = append(line[:0], line[1:]...)
+				c <- true
+			} else {
+				c <- false
+			}
+		case <-done:
+			running = false
+		}
+
+	}
 }
