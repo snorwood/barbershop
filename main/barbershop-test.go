9952c3bba5c197f0c0e61047e8a721d4a8bed247
snorwood <steven@snorwood.com>
2013-11-19 13:30:35 -0500

    Switched to a much more reasonable/sensible approach

--- main/barbershop-test.go
+++ main/barbershop-test.go
@@ -8,36 +8,9 @@ import (
 	"time"
 )
 
-type Person struct {
-	busy bool
-	ID   int
-}
-
-type Barber struct {
-	busy       bool
-	ReturnBusy chan chan bool
-	SetBusy    chan bool
-
-	ID int
-}
-
-func (self *Barber) DataStream() {
-	self.ReturnBusy = make(chan chan bool)
-	self.SetBusy = make(chan bool)
-	for {
-		select {
-		case returnBusy := <-self.ReturnBusy:
-			returnBusy <- self.busy
-		case busy := <-self.SetBusy:
-			self.busy = busy
-		}
-	}
-}
-
-func (self *Barber) GetBusy() bool {
-	c := make(chan bool)
-	self.ReturnBusy <- c
-	return <-c
+type IDGroup struct {
+	BID int
+	CID int
 }
 
 type MyWriter struct {
@@ -64,95 +37,217 @@ var current = time.Now()
 var log *os.File
 var output io.Writer
 
-func spawnCustomer(entrance chan *Person, done chan bool) {
-	for i := 1; i <= 10; i++ {
-		random := time.Duration(rand.Int31n(2)+1) * time.Second
-		<-time.After(random)
-		person := &Person{ID: i}
-		fmt.Fprint(output, "Added new Customer ", i)
-		entrance <- person
-	}
-
-	done <- true
+type Barber struct {
+	log       string
+	busy      bool
+	id        int
+	Stop      chan IDGroup
+	Start     chan int
+	TimeSlept chan chan float32
+	IsBusy    chan chan bool
+	Finish    chan bool
 }
 
-func checkBusy(barbers []*Barber) bool {
-	for _, barber := range barbers {
-		if barber.GetBusy() {
-			return true
+func (self *Barber) GoLive() {
+	var haircutTimer <-chan time.Time = nil
+	finished := false
+	customerID := 0
+	setBusy := make(chan bool)
+	for !finished {
+		select {
+		case <-haircutTimer:
+			haircutTimer = nil
+			go func(stop chan IDGroup, id IDGroup) {
+				stop <- id
+				setBusy <- false
+			}(self.Stop, IDGroup{self.id, customerID})
+
+		case customerID = <-self.Start:
+			self.busy = true
+			haircutTimer = time.After(time.Duration(rand.Int31n(6)+3) * time.Millisecond)
+
+		case timeSlept := <-self.TimeSlept:
+			timeSlept <- 0
+
+		case isBusy := <-self.IsBusy:
+			isBusy <- self.busy
+
+		case self.busy = <-setBusy:
+
+		case <-self.Finish:
+			finished = true
 		}
 	}
-	return false
 }
 
-func HairCut(barber *Barber, person *Person, doneCutting chan chan *Person) {
-	barber.SetBusy <- true
-	fmt.Fprint(output, "Barber ", barber.ID, " is gettin' to work on Customer ", person.ID)
-	<-time.After(time.Duration(rand.Int31n(3)+3) * time.Second)
-	fmt.Fprint(output, "Barber ", barber.ID, " finished haircut on Customer ", person.ID)
-	barber.SetBusy <- false
-	c := make(chan *Person)
-	doneCutting <- c
-	person = (<-c)
-	if person == nil {
-		fmt.Fprint(output, "Barber ", barber.ID, " is snoozin'")
-	} else {
-		go HairCut(barber, person, doneCutting)
+type BarberReader struct {
+	Stop      chan IDGroup
+	Start     chan int
+	TimeSlept chan chan float32
+	IsBusy    chan chan bool
+	ID        int
+	Finish    chan bool
+}
+
+func newBarber(id int, stop chan IDGroup) *BarberReader {
+	start := make(chan int)
+	timeSlept := make(chan chan float32)
+	isBusy := make(chan chan bool)
+	finish := make(chan bool)
+
+	localBarber := BarberReader{
+		Stop:      stop,
+		Start:     start,
+		TimeSlept: timeSlept,
+		IsBusy:    isBusy,
+		ID:        id,
+		Finish:    finish,
+	}
+	barber := &Barber{
+		id:        id,
+		log:       "",
+		busy:      false,
+		Stop:      stop,
+		Start:     start,
+		TimeSlept: timeSlept,
+		IsBusy:    isBusy,
+		Finish:    finish,
+	}
+
+	go barber.GoLive()
+
+	return &localBarber
+}
+
+type Customer struct {
+	log        string
+	id         int
+	TimeWaited chan chan float32
+	Message    chan string
+}
+
+func (self *Customer) GoLive() {
+	for {
+		select {
+		case timeWaited := <-self.TimeWaited:
+			timeWaited <- 0
+		case message := <-self.Message:
+			self.log += message
+			fmt.Println(message)
+		}
 	}
 }
+
+type CustomerReader struct {
+	TimeWaited chan chan float32
+	Message    chan string
+	ID         int
+}
+
+func newCustomer(id int) *CustomerReader {
+	timeWaited := make(chan chan float32)
+	message := make(chan string)
+
+	localCustomer := CustomerReader{ID: id, TimeWaited: timeWaited, Message: message}
+	customer := Customer{log: "", id: id, TimeWaited: timeWaited, Message: message}
+	go customer.GoLive()
+
+	return &localCustomer
+}
+
+var numBarbers int = 20
 
 func main() {
-	os.Create("output.txt")
-	log, _ := os.OpenFile("output.txt", os.O_APPEND, 0666)
-	defer log.Close() //we'll close this file as we leave scope, no matter what
-
-	output = NewMyWriter(os.Stdout, log)
-
-	barbers := make([]*Barber, 3)
-	line := make([]*Person, 0, 10)
-
-	entrance := make(chan *Person, 10)
-	doneCutting := make(chan chan *Person)
-	done := make(chan bool)
-	go spawnCustomer(entrance, done)
-	noMoreCustomers := false
-
-	for i := range barbers {
-		barbers[i] = &Barber{busy: false, ID: i + 1}
-		go barbers[i].DataStream()
+	stop := make(chan IDGroup)
+	barbers := make([]*BarberReader, 3)
+	for id := 1; id <= 3; id++ {
+		barbers[id-1] = newBarber(id, stop)
 	}
 
-	for !noMoreCustomers || checkBusy(barbers) {
+	customers := make([]*CustomerReader, 10)
+	customerCount := 0
+	customersEntered := 0
+	customersServed := 0
+
+	for customersEntered < numBarbers || customerCount > 0 || !allBarbersFinished(barbers) {
+		// newCustomerTimer := time.After(time.Duration(rand.Int31n(1)+1) * time.Second)
+		newCustomerTimer := time.After(time.Millisecond)
+		if customersEntered >= numBarbers {
+			newCustomerTimer = nil
+		}
+
 		select {
-		case person := <-entrance:
-			spotAvailable := false
-			for _, barber := range barbers {
-				if !barber.GetBusy() {
-					spotAvailable = true
-					go HairCut(barber, person, doneCutting)
-					break
+		case <-newCustomerTimer:
+			customersEntered += 1
+			customer := newCustomer(customersEntered)
+			fmt.Println("Customer", customer.ID, "entered.")
+			foundBarber := false
+
+			if customerCount == 0 {
+				for _, barber := range barbers {
+					c := make(chan bool)
+					barber.IsBusy <- c
+					busy := <-c
+					if !busy {
+						foundBarber = true
+						fmt.Println("Barber", barber.ID, "started cutting customer", customer.ID, "'s hair.")
+						barber.Start <- customer.ID
+						break
+					}
 				}
 			}
 
-			if !spotAvailable {
-				if len(line) < cap(line) {
-					fmt.Fprint(output, "Customer ", person.ID, " got in line")
-					line = append(line, person)
-				} else {
-					fmt.Fprint(output, "Customer ", person.ID, " has been turned away")
-				}
+			if customerCount >= 10 && !foundBarber {
+				fmt.Println("Customer", customer.ID, "was turned away.")
+			} else if !foundBarber {
+				customers[customerCount] = customer
+				fmt.Println("Customer", customer.ID, "lined up.")
+				customerCount += 1
 			}
-
-		case c := <-doneCutting:
-			if len(line) > 0 {
-				c <- line[0]
-				line = append(line[:0], line[1:]...)
-			} else {
-				c <- nil
+		case id := <-stop:
+			customersServed += 1
+			fmt.Println("Barber", id.BID, "stopped cutting customer", id.CID, "'s hair.")
+			c := make(chan bool)
+			barbers[id.BID-1].IsBusy <- c
+			busy := <-c
+			if !busy && customerCount > 0 {
+				customer, _ := RemoveCustomer(customers, 0)
+				fmt.Println("Barber", id.BID, "started cutting customer", customer.ID, "'s hair.")
+				barbers[id.BID-1].Start <- customer.ID
+				customerCount -= 1
 			}
-
-		case <-done:
-			noMoreCustomers = true
 		}
 	}
+
+	for _, barber := range barbers {
+		barber.Finish <- true
+	}
+
+}
+
+func RemoveCustomer(customers []*CustomerReader, index int) (*CustomerReader, error) {
+	if index >= len(customers) {
+		return nil, fmt.Errorf("Array index out of bounds")
+	}
+
+	customer := customers[index]
+
+	for visitor := index; visitor < len(customers)-1; visitor++ {
+		customers[visitor] = customers[visitor+1]
+	}
+
+	return customer, nil
+}
+
+func allBarbersFinished(barbers []*BarberReader) bool {
+	for _, barber := range barbers {
+		c := make(chan bool)
+		barber.IsBusy <- c
+		busy := <-c
+		if busy {
+			return false
+		}
+	}
+
+	return true
 }
