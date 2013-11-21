3d4e0f4307a5484c47ada4c21e6f71c2b4d32556
snorwood <steven@snorwood.com>
2013-11-21 16:05:29 -0500

    Basic file server in progress

--- servertest/main.go
+++ servertest/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"net/http"
 )
 
 type Page struct {
@@ -25,9 +26,24 @@ func loadPage(title string) (*Page, error) {
 	}
 }
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+
+	var p *Page = new(Page)
+	var err error = nil
+
+	if p, err = loadPage(title); err != nil {
+		p = &Page{Title: title, Body: []byte(err.Error())}
+	}
+
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
 func main() {
-	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-	p1.save()
-	p2, _ := loadPage("TestPage")
-	fmt.Println(string(p2.Body))
+	http.HandleFunc("/view/", viewHandler)
+	http.ListenAndServe(":8080", nil)
 }
